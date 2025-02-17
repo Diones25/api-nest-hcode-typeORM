5d57ee256dc99a76680513b9ccc38e756ead2296
Diones <pereiradiones987@gmail.com>
2025-02-17 16:41:56 -0300

    configurando typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: '',
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.ENV === "development" ? true : false, //Essa linha deve ficar 'false' em produção
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
