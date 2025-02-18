894a0a9131a96651089a074e01c32cd667bb98f3
Diones <pereiradiones987@gmail.com>
2025-02-18 11:30:17 -0300

    adicionando configuração de variável de ambiente e corrigindo o nome de usuário de DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,17 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
+      username: process.env.DB_USER_NAME,
       password: '',
       database: process.env.DB_DATABASE,
       entities: [UserEntity],
